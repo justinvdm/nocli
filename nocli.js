677252b6660ca6566fbdc8cc11c2df7ac921e072
justinvdm <justinvdm.0.1.0@gmail.com>
2016-03-03 23:47:27 +0200

    expect given module to be a filepath

--- nocli.js
+++ nocli.js
@@ -3,10 +3,10 @@
 let cli = require('yargs');
 let yaml = require('js-yaml').safeLoad;
 let open = require('fs').readFileSync;
+let resolve = require('path').resolve;
 let _ = require('lodash');
 let extend = _.extend;
 let omit = _.omit;
-let relative = require('require-relative');
 
 
 let args = cli
@@ -21,4 +21,4 @@ let opts = {};
 if (args.c) extend(opts, yaml(open(args.c)));
 extend(opts, omit(args, 'c', '_', 'help', '$0', 'nocli.js'));
 
-relative(args._[0], process.cwd())(opts);
+require(resolve(process.cwd(), args._[0]))(opts);
